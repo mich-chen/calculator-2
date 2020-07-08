c8e82e1c26fa5f1e4b5f7b644afa3c82a99e3fcd
Michelle Chen <mich.chen.94@gmail.com>
2020-07-08 22:48:28 +0000

    instructions and psuedocode

--- calculator.py
+++ calculator.py
@@ -4,3 +4,19 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
+# split user's input string into (prefix_opperand, num1, num2)
+# example: 
+# if the first token is 'pow':
+    # call power function with the other two tokens (num1, num2)
+
+"""repeat forever:
+    read input
+    tokenize input
+        if the first token is "q":
+            quit
+        else:
+            (decide which math function to call based on first token)
+            if the first token is 'pow':
+                  call the power function with the other two tokens
+
+            (...etc.)"""
